f2949988ea7aaeba60ca43e1ac346d646f678e10
“Cornel <info@chitanu.com>
2019-01-29 15:26:26 +0200

    Add first unit test to the Android RN native module.

---
+++ src/android/src/test/java/com/reactlibrary/FidelActivityEventListenerTest.java
@@ -0,0 +1,11 @@
+package com.reactlibrary;
+
+import junit.framework.TestCase;
+
+public class FidelActivityEventListenerTest extends TestCase {
+
+    public void test_WhenRequestCodeIsNotFidelCode_DontCall() {
+        FidelActivityEventListener sut = new FidelActivityEventListener();
+        sut.onActivityResult(null,0,0,null);
+    }
+}
