a2b5e0a13626c1ae84ee4be1ba63fe48a716de61
Corneliu Chitanu <info@chitanu.com>
2019-09-22 07:36:45 +0300

    Integrates better with Cocoapods. Finds React dependencies in more contexts.

--- ios/FLRNImageFromRNAdapter.m
+++ ios/FLRNImageFromRNAdapter.m
@@ -7,7 +7,14 @@
 //
 
 #import "FLRNImageFromRNAdapter.h"
+
+#if __has_include(<React/RCTConvert.h>)
+#import <React/RCTConvert.h>
+#elif __has_include("RCTConvert.h")
 #import "RCTConvert.h"
+#else
+#import "React/RCTConvert.h" // Required when used as a Pod in a Swift project
+#endif
 
 @implementation FLRNImageFromRNAdapter
 
--- ios/RCTConvert+Country.h
+++ ios/RCTConvert+Country.h
@@ -6,8 +6,22 @@
 //  Copyright © 2019 Facebook. All rights reserved.
 //
 
+//#import "RCTConvert.h"
+#if __has_include(<React/RCTConvert.h>)
+#import <React/RCTConvert.h>
+#elif __has_include("RCTConvert.h")
 #import "RCTConvert.h"
+#else
+#import "React/RCTConvert.h" // Required when used as a Pod in a Swift project
+#endif
+
+#if __has_include(<Fidel/Fidel-Swift.h>)
+#import <Fidel/Fidel-Swift.h>
+#elif __has_include("Fidel-Swift.h")
 #import "Fidel-Swift.h"
+#else
+#import "Fidel/Fidel-Swift.h" // Required when used as a Pod in a Swift project
+#endif
 
 #define FLCountryValues @{@"unitedKingdom" : @(FLCountryUnitedKingdom), @"ireland" : @(FLCountryIreland), @"unitedStates" : @(FLCountryUnitedStates), @"sweden" : @(FLCountrySweden), @"japan" : @(FLCountryJapan), @"canada" : @(FLCountryCanada), @"noDefault" : @(FLCountryNoDefault)}
 
