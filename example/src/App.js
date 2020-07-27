e7891a055097b1c2ead15f0c76c2dbe75db2255f
Corneliu Chitanu <tap@dolfn.com>
2020-07-27 23:22:44 +0200

    Update the example project.

--- example/src/App.js
+++ example/src/App.js
@@ -39,7 +39,8 @@ export default class App extends Component<Props> {
       programId: 'Your Program ID'
     })
     Fidel.setOptions ({
-      country: Fidel.Country.canada,
+      bannerImage: resolvedImage,
+      // country: Fidel.Country.canada,
       supportedCardSchemes: Array.from(cardSchemes),
       autoScan: false,
       metaData: {'meta-data-1': 'value1'},
