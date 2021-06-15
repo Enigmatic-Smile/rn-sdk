59faa47e0712349394c938606cc05e4c2180fd24
Corneliu Chitanu <tap@dolfn.com>
2021-06-15 21:55:34 +0300

    Add script that installs the local library code in the example project.

---
+++ scripts/installLocalLibrary.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the version of the library
+version=`ruby -rjson -e 'j = JSON.parse(File.read("package.json")); puts j["version"]'`
+
+# Make a TGZ package of the library
+npm pack
+
+# Get the TGZ package file name
+localLibraryPackage=fidel-react-native-$version.tgz
+
+# Switch to the example project, in order to install the library in the example project
+cd example
+
+# Install the local library code in the example project, from the TGZ package
+yarn add file:../$localLibraryPackage
